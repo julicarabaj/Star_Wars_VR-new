8fae661cc906abc1e6c3ab7d448686f26db6dc49
julicarabaj <127246404+julicarabaj@users.noreply.github.com>
2023-09-29 14:19:37 -0300

    lightsaber sound :)

--- Assets/Scripts/Lightsaber.cs
+++ Assets/Scripts/Lightsaber.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class Lightsaber : MonoBehaviour
 {
+    public OVRInput.Controller controller;
     private bool activate = false;
     private GameObject laser;
     private Vector3 fullSize;
+    private AudioSource source;
+    public AudioClip audioclip;
+    public AudioClip audioHum;
 
     void Start()
     {
+        source = gameObject.AddComponent<AudioSource>();
+        source.spatialBlend = 1;
+        source.volume = 0.3f;
         laser = transform.Find("SingleLine-TextureAdditive").gameObject;
         fullSize = laser.transform.localScale;
         laser.transform.localScale = new Vector3(fullSize.x, 0, fullSize.z);
@@ -17,12 +24,29 @@ public class Lightsaber : MonoBehaviour
 
     void Update()
     {
-        if (OVRInput.Get(OVRInput.Button.One)) 
+        InputController();
+        LaserController();
+        var velocity = OVRInput.GetLocalControllerAngularVelocity(controller);
+        if (velocity.magnitude > 1)
+        {
+            source.PlayOneShot(audioclip);
+        }
+        else if (source.isPlaying == false)
+        {
+            source.PlayOneShot(audioHum);
+        }
+    }
+    void InputController ()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))//(OVRInput.Get(OVRInput.Button.One))
         {
             Debug.Log("Botón One presionado");
             activate = !activate;
         }
 
+    }
+    void LaserController ()
+    {
         if (activate && laser.transform.localScale.y < fullSize.y)
         {
             laser.transform.localScale += new Vector3(0, 0.0001f, 0);
@@ -31,5 +55,5 @@ public class Lightsaber : MonoBehaviour
         {
             laser.transform.localScale += new Vector3(0, -0.0001f, 0);
         }
-    }
+    }   
 }
