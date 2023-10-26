19565d11d4dc8965f8d0fe46871549e424541083
julicarabaj <127246404+julicarabaj@users.noreply.github.com>
2023-10-26 15:56:52 -0300

    no hicimos una mierda

---
+++ Assets/Scripts/HealthScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float maxhealth;
+    public float health;
+    public bool isAlive = true;
+    void Start()
+    {
+        health = maxhealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void TakeDamage(float damage)
+    {
+        if(isAlive)
+        {
+            health -= damage;
+            if (health <= 0)
+            {
+                //murio
+                death();
+            }
+            else
+            {
+                //animacion de recibir daño
+            }
+        }
+    }
+    void death()
+    {
+        //todos los elementos que se disparan cuando la salud llega a 0
+        Debug.Log("Death");
+        //animacion muerte
+    }
+}
