b05fa6bdca58242313048ccc1627a8d5a57b100e
julicarabaj <127246404+julicarabaj@users.noreply.github.com>
2023-11-03 14:30:48 -0300

    script enemy health

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -6,19 +6,43 @@ using UnityEngine;
 public class EnemyHealth : MonoBehaviour
 {
     private int golpesRecibidos = 0;
+    private bool isDead = false;
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
+        if (isDead) return; // Si el enemigo ya está muerto, no procesar más colisiones.
+
         if (collision.gameObject.tag == "Lightsaber")
         {
             golpesRecibidos++;
             if (golpesRecibidos >= 10)
             {
-                // El jugador gana el juego
-                Debug.Log("Has ganado. Le pegaste 5 veces a Darth Vader.");
-                Destroy(gameObject);
+                // Ejecutar la animación de muerte si el enemigo ha recibido suficientes golpes.
+                animator.SetTrigger("Death");
+
+                // Esperar a que la animación de muerte termine antes de destruir el objeto del enemigo.
+                StartCoroutine(DestroyEnemyAfterAnimation());
             }
         }
     }
 
+    private IEnumerator DestroyEnemyAfterAnimation()
+    {
+        // Ajusta el tiempo de espera según la duración de tu animación de muerte.
+        yield return new WaitForSeconds(2.0f); // Espera 2 segundos (ajusta el valor según tu animación).
+
+        // El jugador gana el juego
+        Debug.Log("Has ganado. Le pegaste 10 veces a Darth Vader.");
+
+        // Destruye el objeto del enemigo.
+        Destroy(gameObject);
+
+        isDead = true; // Marcar al enemigo como muerto para evitar procesar más colisiones.
+    }
 }
