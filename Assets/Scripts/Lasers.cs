6d3109d75ee641dcd1fa6b327f1f43709cb5b47f
julicarabaj <127246404+julicarabaj@users.noreply.github.com>
2023-11-21 12:06:36 -0300

    cositas como lasers y health bar y transiciones

--- Assets/Scripts/Lasers.cs
+++ Assets/Scripts/Lasers.cs
@@ -4,31 +4,36 @@ using UnityEngine;
 
 public class Lasers : MonoBehaviour
 {
+    public Rigidbody rb;
     public float speed;
     public float lifeTime;
     public GameObject ExplosionEffect;
     private AudioSource source;
     public AudioClip clash;
+    public Vector3 laserVelocity;
     void Start()
     {
       Destroy(gameObject, lifeTime);
       source = gameObject.AddComponent<AudioSource>();
       source.spatialBlend = 1;
       source.volume = 8f;
+      rb = GetComponent<Rigidbody>();
+      rb.velocity = laserVelocity;
     }
 
     void Update()
     {
-        transform.position += transform.forward * speed * Time.deltaTime;
+        // transform.position += transform.forward * speed * Time.deltaTime;
+        
     }
 
     void OnCollisionEnter(Collision col)
     {
         if (col.gameObject.CompareTag("Lightsaber"))
         {
-            //Instantiate(ExplosionEffect, transform.position, transform.rotation);
+            Instantiate(ExplosionEffect, transform.position, transform.rotation);
             Destroy(gameObject);
-            Debug.Log("¡Me toco!");
+            Debug.Log("ï¿½Me toco!");
             if (source != null && source.enabled)
             {
                 source.PlayOneShot(clash);
--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     private int disparosRecibidos = 0;
     private AudioSource source;
     public AudioClip hit; //este es el sonido
+    public Image barraDeVida;
+    public float vidaActual = 100;
+    public float vidaMaxima = 100;
 
     private void Start()
     {
         source = gameObject.AddComponent<AudioSource>();
         source.spatialBlend = 1;
         source.volume = 3f;
+    }
+    private void Update()
+    {
+        barraDeVida.fillAmount = vidaActual / vidaMaxima;
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -22,11 +30,13 @@ public class PlayerHealth : MonoBehaviour
             disparosRecibidos++;
             source.PlayOneShot(hit);
             Debug.Log("Sonido!");
+            vidaActual -= 10;
+           // barraDeVida.fillAmount = vidaActual / vidaMaxima;
             //if (hit != null && source != null)
-          //  {
+            //  {
             //    source.PlayOneShot(hit);
             //}
-            if (disparosRecibidos >= 5)
+            if (disparosRecibidos >= 10)
             {
                  //moris
             }
